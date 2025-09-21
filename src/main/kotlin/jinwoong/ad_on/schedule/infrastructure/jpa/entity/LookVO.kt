02c21f15aa772c9becd9e12967ae8c8c0e7899c0
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-09-22 02:31:31 +0900

    fix: jpa error 수정
    - 중첨 embed 구조로 인한 오류 수정

--- src/main/kotlin/jinwoong/ad_on/schedule/infrastructure/jpa/entity/LookVO.kt
+++ src/main/kotlin/jinwoong/ad_on/schedule/infrastructure/jpa/entity/LookVO.kt
@@ -5,19 +5,19 @@ import jakarta.persistence.Embeddable
 
 @Embeddable
 data class LookVO(
-    @Column(name = "image_url", length = 2000)
+    @Column(table = "creative", name = "image_url", length = 2000)
     var imageURL: String? = null,
 
-    @Column(name = "movie_url", length = 2000)
+    @Column(table = "creative", name = "movie_url", length = 2000)
     var movieURL: String? = null,
 
-    @Column(name = "logo_url", length = 2000)
+    @Column(table = "creative", name = "logo_url", length = 2000)
     var logoURL: String? = null,
 
-    @Column(name = "copyrighting_title")
+    @Column(table = "creative", name = "copyrighting_title")
     var copyrightingTitle: String? = null,
 
-    @Column(name = "copyrighting_subtitle")
+    @Column(table = "creative", name = "copyrighting_subtitle")
     var copyrightingSubtitle: String? = null,
 
-    )
+)
